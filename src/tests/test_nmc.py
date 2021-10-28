726f78f962deb6a309132658a869352e48f0a978
Giuseppe <gf1997.gf@gmail.com>
2021-10-28 16:39:53 +0200

    test on data perturb

--- src/tests/test_nmc.py
+++ src/tests/test_nmc.py
@@ -1,8 +1,8 @@
 import unittest
 
 import numpy as np
-
 from src.nmc import NMC
+from src.data_perturb import CDataPerturbRandom
 
 
 class TestNMC(unittest.TestCase):
@@ -11,10 +11,19 @@ class TestNMC(unittest.TestCase):
         n_samples = 100
         n_features = 200
         self.x = np.zeros(shape=(n_samples, n_features))
-        self.y = np.zeros(shape=(n_samples,))
+        self.y = np.ones(shape=(n_samples,))
+        self.clf = NMC()
+
+    def test_init(self):
+        "Check if centroids are None right after creation"
+        self.assertTrue(self.clf.centroid is None)
 
     def test_fit(self):
-        pass
+        self.clf.fit(self.x, self.y)
+        n_classes = np.unique(self.y).size
+        expected_centroid_shape = (n_classes, self.x.shape[1])
+        self.assertEqual(self.clf.centroid.shape, expected_centroid_shape)
+
 
     def test_predict(self):
         pass
